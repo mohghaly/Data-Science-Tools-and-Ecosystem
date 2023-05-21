e43097d323751c16be53e9d1e15ba19a14253037
mohghaly <35000880+mohghaly@users.noreply.github.com>
2023-05-21 20:20:18 +0300

    Add files via upload

---
+++ DataScienceEcosystem.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# #  Data Science Tools and Ecosystem
+
+# In this notebook, Data Science Tools and Ecosystem are summarized.
+
+# **Objectives:**
+# some of the key takeaways from the course
+# - Languages used for data science
+# - how to create a table in Jupiter
+# - Simple Arithmatic operations used in Jupiter
+# 
+
+# Some of the popular languages that Data Scientists use are:
+
+# 1. Python
+# 2. R
+# 3. Julia
+
+# |Data Science Tools|
+# |------------------|
+# |Haddop|
+# |Tensorflow|
+# |Pandas|
+
+# ### Below are a few examples of evaluating arithmetic expressions in Python
+
+# In[5]:
+
+
+# This a simple arithmetic expression to mutiply then add integers
+(3*4)+5
+
+
+# In[6]:
+
+
+#to convert minutes to hours divide the number of minuted by 60, in this example we have 200 minutes
+200/60
+
+
+# ## Author
+# Mohaed Ghaly
+
+# **Github Link:**
+# <a href ="https://www.coursera.org/learn/open-source-tools-for-data-science/home/">**Github Link:**</a>
